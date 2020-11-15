a430775f69d592c8cd0cb83e0744a93e83f17219
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-11-15 16:48:44 +0900

    End Section1 of Creating and Combining Views

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by AzumaSato on 2020/11/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
