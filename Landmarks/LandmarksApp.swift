4551c81579b78c515775e3acfec00b070e1f2943
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-11-19 23:35:07 +0900

    Add ImageStore and Landmark

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -11,7 +11,6 @@ import SwiftUI
 struct LandmarksApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
         }
     }
 }
--- Landmarks/Models/Landmark.swift
+++ Landmarks/Models/Landmark.swift
@@ -2,7 +2,41 @@
 //  Landmark.swift
 //  Landmarks
 //
-//  Created by AzumaSato on 2020/11/18.
+//  Created by AzumaSato on 2020/11/16.
 //
 
-import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    fileprivate var imageName: String
+    fileprivate var coordinates: Coordinates
+    var state: String
+    var park: String
+    var category: Category
+
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+    enum Category: String, CaseIterable, Codable, Hashable {
+        case featured = "Featured"
+        case lakes = "Lakes"
+        case river = "Rivers"
+    }
+}
+
+extension Landmark {
+//    var image: Image {
+//        ImageStore.shared.image(name: imageName)
+//    }
+}
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
