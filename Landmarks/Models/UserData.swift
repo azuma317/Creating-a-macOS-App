4c83190d96a93461ed699783171b217e178cc837
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-11-21 16:20:55 +0900

    Add UserData

---
+++ Landmarks/Models/UserData.swift
@@ -0,0 +1,20 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by AzumaSato on 2020/11/21.
+//
+
+import SwiftUI
+
+struct UserData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserData_Previews: PreviewProvider {
+    static var previews: some View {
+        UserData()
+    }
+}
