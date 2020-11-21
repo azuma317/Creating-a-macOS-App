9a335a5714eba9aaa1dba33ee779e9ae43c07f23
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-11-21 17:27:31 +0900

    Add Badge and BadgeSymbol

---
+++ Landmarks/HexagonParameters.swift
@@ -0,0 +1,20 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by AzumaSato on 2020/11/21.
+//
+
+import SwiftUI
+
+struct HexagonParameters: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HexagonParameters_Previews: PreviewProvider {
+    static var previews: some View {
+        HexagonParameters()
+    }
+}
---
+++ Landmarks/Supporting Views/Badge.swift
@@ -0,0 +1,28 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by AzumaSato on 2020/11/21.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    static let rotationCount = 8
+
+//    var badgeSymbols: some View {
+//        ForEach(0.<Badge.rotationCount) { i in
+//            RotatedBadge
+//        }
+//    }
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
