26535310a9c24134de35accb520199fd239aeaf9
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-11-25 23:28:16 +0900

    Add GraphCapsule and HikeGraph

---
+++ Landmarks/Supporting Views/GraphCapsule.swift
@@ -0,0 +1,20 @@
+//
+//  GraphCapsule.swift
+//  Landmarks
+//
+//  Created by AzumaSato on 2020/11/24.
+//
+
+import SwiftUI
+
+struct GraphCapsule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GraphCapsule_Previews: PreviewProvider {
+    static var previews: some View {
+        GraphCapsule()
+    }
+}
---
+++ Landmarks/Supporting Views/HikeGraph.swift
@@ -0,0 +1,20 @@
+//
+//  HikeGraph.swift
+//  Landmarks
+//
+//  Created by AzumaSato on 2020/11/24.
+//
+
+import SwiftUI
+
+struct HikeGraph: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HikeGraph_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeGraph()
+    }
+}
