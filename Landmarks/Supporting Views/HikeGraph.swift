784af2db8ded622c26f29e57a4e76e4034cbb278
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-11-26 23:44:08 +0900

    Update body in HikeGraph

--- Landmarks/Supporting Views/HikeGraph.swift
+++ Landmarks/Supporting Views/HikeGraph.swift
@@ -45,7 +45,27 @@ struct HikeGraph: View {
     }
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        let data = hike.observations
+        let overallRange = rangeOfRanges(data.lazy.map { $0[keyPath: self.path] })
+        let maxMagnitude = data.map { magnitude(of: $0[keyPath: path]) }.max()!
+        let heightRatio = (1 - CGFloat(maxMagnitude / magnitude(of: overallRange))) / 2
+
+        return GeometryReader { proxy in
+            HStack(alignment: .bottom, spacing: proxy.size.width / 20) {
+                ForEach(data.indices) { index in
+                    GraphCapsule(
+                        index: index,
+                        height: proxy.size.height,
+                        range: data[index][keyPath: self.path],
+                        overallRange: overallRange
+                    )
+                    .colorMultiply(self.color)
+                    .transition(.slide)
+                    .animation(.ripple(index: index))
+                }
+                .offset(x: 0, y: proxy.size.height * heightRatio)
+            }
+        }
     }
 }
 
